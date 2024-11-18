700f47ccc71cc94e1b00e3ed312e64c0be82cdaf
Infinnitor <infinnitor@gmail.com>
2024-11-18 23:06:47 +0000

    Update bashrc

--- bash/rc.sh
+++ bash/rc.sh
@@ -2,6 +2,9 @@ export PATH="$PATH:/home/computer/.local/bin/"
 export TERMINAL=alacritty
 export EDITOR="vim"
 
+bind '"\e[A":history-search-backward'
+bind '"\e[B":history-search-forward'
+
 PS1="\[\033[01;31m\]\w \$ \[\033[00m\]"
 tabs -4
 (cat ~/.cache/wal/sequences &)
